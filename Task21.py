3fe1c1bbb6259b8d06163b62307a330226f0a911
User <user@Users-MacBook-Pro-235.local>
2019-12-21 01:05:13 -0600

    first commit

---
+++ Task21.py
@@ -0,0 +1,9 @@
+# Напишите функцию которая будет суммировать введенные три случайные цифры.
+
+def multiply(num1, num2, num3):
+    answer = num1 * num2 * num3
+    return answer
+
+print(multiply(2, 2, 2))
+
+
